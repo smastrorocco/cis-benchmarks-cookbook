269e6d9a7084e8c5f0c5d53ed387e4e81e32836f
Steve Mastrorocco <stephen.mastrorocco@gmail.com>
2017-11-12 20:11:50 -0500

    Include benchmark recipe

--- recipes/default.rb
+++ recipes/default.rb
@@ -2,4 +2,6 @@
 # Cookbook:: cis-benchmarks
 # Recipe:: default
 #
-# Copyright:: 2017, The Authors, All Rights Reserved.
+# Copyright:: 2017, Steve Mastrorocco, All Rights Reserved.
+
+include_recipe "cis-benchmarks::#{node['cis-benchmarks']['benchmark']}"
