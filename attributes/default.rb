27da0b15a2e6d503fce29969e5c70885c516a7a7
Steve Mastrorocco <stephen.mastrorocco@gmail.com>
2017-11-22 16:32:55 -0500

    Select benchmark OS from ohai attributes

--- attributes/default.rb
+++ attributes/default.rb
@@ -5,5 +5,10 @@
 # Copyright:: 2017, Steve Mastrorocco, All Rights Reserved.
 
 # CIS Benchmark to apply
-# TODO: Apply based on ohai attributes
-default['cis-benchmarks']['benchmark'] = ''
+default['cis-benchmarks']['benchmark'] = case node['platform']
+                                         when 'windows'
+                                           case node['platform_version']
+                                           when /6.3.\d+/
+                                             'windows_server_2012r2'
+                                           end
+                                         end
