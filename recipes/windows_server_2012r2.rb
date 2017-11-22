c4aa77fbd26c8e562f1989aec247e306c7175c34
Steve Mastrorocco <stephen.mastrorocco@gmail.com>
2017-11-22 16:33:51 -0500

    Added resource to process registry keys and update description of built in users

--- recipes/windows_server_2012r2.rb
+++ recipes/windows_server_2012r2.rb
@@ -40,3 +40,44 @@ powershell_script "secedit configure #{prefix}" do
     if ($log -match 'Mismatch') { return $true } else { return $false }
   EOH
 end
+
+# Apply registry settings
+reg_keys = node['cis-benchmarks']['windows_server_2012r2']['security_policy']['registry_keys']
+
+reg_keys.each do |k, v|
+  next if v.nil?
+
+  registry_key "CIS #{k}" do
+    key v['name']
+    values [
+      {
+        name: v['values']['name'],
+        type: v['values']['type'],
+        data: v['values']['data'],
+      },
+    ]
+    recursive true
+  end
+end
+
+# Alter Administrator and Guest descriptions from default
+new_admin_user = node['cis-benchmarks']['windows_server_2012r2']['security_policy']['system_access']['NewAdministratorName']
+new_admin_desc = node['cis-benchmarks']['windows_server_2012r2']['security_policy']['NewAdministratorDescription']
+new_guest_user = node['cis-benchmarks']['windows_server_2012r2']['security_policy']['system_access']['NewGuestName']
+new_guest_desc = node['cis-benchmarks']['windows_server_2012r2']['security_policy']['NewGuestDescription']
+
+powershell_script "Set #{new_admin_user} account description" do
+  code "NET USER #{new_admin_user} /COMMENT:'#{new_admin_desc}'"
+  not_if <<-EOH
+    $adminUser = Get-WmiObject Win32_UserAccount | Where { ($_.LocalAccount -eq $true) -and ($_.Name -eq '#{new_admin_user}') }
+    if (($adminUser -eq $null) -or ($adminUser.Description -eq '#{new_admin_desc}')) { return $true } else { return $false }
+  EOH
+end
+
+powershell_script "Set #{new_guest_user} account description" do
+  code "NET USER #{new_guest_user} /COMMENT:'#{new_guest_desc}'"
+  not_if <<-EOH
+    $adminUser = Get-WmiObject Win32_UserAccount | Where { ($_.LocalAccount -eq $true) -and ($_.Name -eq '#{new_guest_user}') }
+    if (($adminUser -eq $null) -or ($adminUser.Description -eq '#{new_guest_desc}')) { return $true } else { return $false }
+  EOH
+end
