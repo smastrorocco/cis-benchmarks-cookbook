18e2e6d9e2704d793fbbaf2beaf6eb68cf91bb2f
Steve Mastrorocco <stephen.mastrorocco@gmail.com>
2018-03-14 06:56:34 -0400

    Updated license and Chef client version

--- metadata.rb
+++ metadata.rb
@@ -1,11 +1,11 @@
 name                'cis-benchmarks'
 maintainer          'Steve Mastrorocco'
 maintainer_email    'stephen.mastrorocco@gmail.com'
-license             'All Rights Reserved'
+license             'MIT'
 description         'Installs/Configures cis-benchmarks'
 long_description    'Installs/Configures cis-benchmarks'
 version             '0.1.0'
-chef_version        '>= 12.1' if respond_to?(:chef_version)
+chef_version        '>= 13.0' if respond_to?(:chef_version)
 
 # Source and issues URL
 source_url 'https://github.com/smastrorocco/cis-benchmarks-cookbook'
